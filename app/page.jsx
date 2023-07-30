b54fca65200fa046bbf43a367bd1f27ca33b162c
GarcelCode <garcelcode@gmail.com>
2023-07-30 00:35:07 -0600

    [ADD] Log in page and scss library

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,29 @@
-import Image from 'next/image'
-import Link from 'next/link'
+"use client";
+import style from "./loginpage.module.scss";
 
 export default function Home() {
-  return ( <>
-    <Link href="/movies">Movies</Link>
-    <Link href="/videos">Videos</Link>
-    </>
-  )
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    window.location = "/menu";
+  };
+
+  return (
+    <div className={style.MainContainer}>
+      <form className={style.Form} onSubmit={handleSubmit}>
+        <input
+          className={style.FormInput}
+          name="username"
+          type="text"
+          placeholder="Type your username"
+        />
+        <input
+          className={style.FormInput}
+          name="password"
+          type="password"
+          placeholder="Type your password"
+        />
+        <input className={style.SubmitBtn} type="submit" value="Log in" />
+      </form>
+    </div>
+  );
 }
